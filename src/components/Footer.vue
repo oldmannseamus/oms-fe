64bfbfa7de5f1d3fd4a21cb341540a621d493db5
James Thompson <oldmannseamus@live.com>
2025-07-03 23:04:09 -0500

    Break out layout and header components for better organization. Remove unused install components.

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+    const year = new Date().getFullYear();
+</script>
+
+<template>
+  &copy; {{ year }} James Thompson 
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Navigation from './Navigation.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+    <h2 class="green">James Thompson</h2>
+    <h3>
+      Engineering Leader
+    </h3>
+    <Navigation />
+  </div>
+</template>
