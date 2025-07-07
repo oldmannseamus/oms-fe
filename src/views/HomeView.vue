244a68df6eaebb00d757b66fe53b9ec759ff9f07
James Thompson <oldmannseamus@live.com>
2025-07-06 22:43:54 -0500

    Revise color schemes and branding.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,8 @@
 </script>
 
 <template>
-  <main>
-    <h1>Hi. I&apos;m James.</h1>
-    <p>I&apos;m a software developer based in Chicago.</p>
-    <p>My specialties are user interface development, web-native computer graphics, and engineering management.</p>
-    <p>You can view my resume <RouterLink to='/profile'>here</RouterLink>, or learn more about my work <RouterLink to='/projects'>here</RouterLink>.</p>
-  </main>
+  <h1 class="green">Hi. I&apos;m James.</h1>
+  <p>I&apos;m a software developer based in Chicago.</p>
+  <p>My specialties are user interface development, web-native computer graphics, and engineering management.</p>
+  <p>You can view my resume <RouterLink to='/profile'>here</RouterLink>, or learn more about my work <RouterLink to='/projects'>here</RouterLink>.</p>
 </template>
