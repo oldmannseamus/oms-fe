3e432214c25bf85af07d1154679056dba6589db1
James Thompson <oldmannseamus@live.com>
2025-07-29 11:39:13 -0500

    Remove scoped styles from unfinished views.

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,13 +3,3 @@
     <h1>This is a projects page</h1>
   </div>
 </template>
-
-<style>
-  @media (min-width: 1024px) {
-    .projects {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-</style>
