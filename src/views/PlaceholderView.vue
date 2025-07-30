7b5a9fe1e530941e398bc8915f0bf0824fc1e0fc
James Thompson <oldmannseamus@live.com>
2025-07-29 21:52:35 -0500

    Create placeholder view for temporary use.

---
+++ src/views/PlaceholderView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import ExternalLink from '@/components/ExternalLink.vue';
+</script>
+
+<template>
+  <h1 class="green">Hi. I&apos;m James.</h1>
+  <p>I&apos;m a software developer based in Chicago.</p>
+  <p>My specialties are user interface development, web-native computer graphics, and engineering management.</p>
+  <p>You can connect with me on
+    <ExternalLink href="https://www.linkedin.com/in/james-thompson-bb8ba81b/" noReferrer>LinkedIn</ExternalLink>
+  to learn more.</p>
+</template>
